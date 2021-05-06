d70b6d7c007914919220bc1ed01c7d2ffdc90a78
Kevin Su <kevinsu@outlook.co.nz>
2021-05-06 14:11:56 +1000

    small changes

--- src/App.js
+++ src/App.js
@@ -28,44 +28,49 @@ class App extends React.Component {
   }
 
   render(){
+      const isLoading2 = this.state.isLoading;
       return (
-        <div>
+         <div>
             <h1 style = {{marginTop:"50px"}}>{this.state.number} {this.state.term}</h1>
             <hr></hr>
-            <section className = "section_style">
-                {this.state.articles.map((article) => {
-                    const {
-                        abstract,
-                        headline:{ main },
-                        byline:{ original },
-                        lead_paragraph,
-                        news_desk,
-                        section_name,
-                        web_url,
-                        _id,
-                        word_count,
-                        pub_date,
-                    } = article;
+            {isLoading2?<div style = {{minHeight:"300px", textAlign:"center"}}><h3>Is Loading Please Wait...</h3></div>:
+            <div>
+                <section className = "section_style">
+                    {this.state.articles.map((article) => {
+                        const {
+                            abstract,
+                            headline:{ main },
+                            byline:{ original },
+                            lead_paragraph,
+                            news_desk,
+                            section_name,
+                            web_url,
+                            _id,
+                            word_count,
+                            pub_date,
+                        } = article;
 
-                    return (
-                        <article key = {_id} className = "padded">
-                            <h2 className = "head_title">{main}</h2>
-                            <h4 className = "summary">{abstract}</h4>
-                            <p className = "text_body">{lead_paragraph}</p>
-                            <p className = "text_body" style = {{marginBottom: "150px"}}>Published on {pub_date.substring(0,10)}</p>
-                            <div className = "end">
-                                <ul >
-                                    <li className = "text_body">{original}</li>
-                                    <li className = "text_body">From: {news_desk}</li>
-                                    <li className = "text_body">Section: {section_name}</li>
-                                    <li className = "text_body">Words: {word_count}</li>
-                                </ul>
-                                <a href = {web_url} target = "_blank" class = "button">View Source</a>
-                            </div>
-                        </article>
-                    );
-                })}
-            </section>
+                        return (
+                            <article key = {_id} className = "padded">
+                                <h2 className = "head_title">{main}</h2>
+                                <h4 className = "summary">{abstract}</h4>
+                                <p className = "text_body">{lead_paragraph}</p>
+                                <p className = "text_body" style = {{marginBottom: "150px"}}>Published on {pub_date.substring(0,10)}</p>
+                                <div className = "end">
+                                    <ul >
+                                        {original === ""? <li className = "text_body">By Unknown</li> : <li className = "text_body">{original}</li>}
+                                        {news_desk === ""? <li className = "text_body">From: Unknown</li> : <li className = "text_body">From: {news_desk}</li>}
+                                        {section_name === ""?<li className = "text_body">Section: Unknown</li> : <li className = "text_body">Section: {section_name}</li>}
+                                        {word_count === 0? <li className = "text_body">Video</li> : <li className = "text_body">Words: {word_count}</li>}
+                                    </ul>
+                                    <a href = {web_url} target = "_blank" class = "button">View Source</a>
+                                </div>
+                            </article>
+                        );
+                    })}
+                </section>
+            </div>
+            }
         </div>
       );
    }
